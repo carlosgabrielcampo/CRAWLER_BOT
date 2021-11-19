3c046a01d6f51c7c49fbcee8b72a85d5d88fa5f8
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-11-19 13:25:42 -0300

    test completed

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,16 +6,26 @@ describe('testing Correios webPage', () => {
   beforeAll(async () => {
     await page.goto(cs.pageAdd);
   });
+
   it('the page connects', async () => {
     await expect(page).resolves;
   });
+
   it(`the CEP input id is ${cs.typeInput}`, async () => {
     await page.type(cs.typeInput, '72593-114');
     await expect(page).resolves;
   });
+
   it(`the search button id is ${cs.clickInput}`, async () => {
     await page.click(cs.clickInput);
     await expect(page).resolves;
   });
-  
+
+  it('when passing the correct CEP, the page shows a table with the CEP info', async () => {
+    await page.goto(cs.pageAdd);
+    await page.type(cs.typeInput, '72593-114');
+    await page.click(cs.clickInput);
+    await page.waitForSelector(cs.waitElem)
+    await expect(page).resolves
+  })
 });
